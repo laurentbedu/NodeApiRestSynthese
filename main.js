b3cebb2586ce439ef1ff3696185e3ccf569882bd
laurentbedu <bedulaurent@gmail.com>
2022-03-08 10:53:40 +0100

    starter app

---
+++ main.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const app = express();
+
+const cors = require("cors");
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+
+const config = require("./api/configs")("app");
+app.listen(config.PORT, () => {
+  console.log(`Server is running on port ${config.PORT}.`);
+});
